495a5ef80ceb13935c4c3282e064bb78fa6edd1b
abdesslem <amriabdesslem@gmail.com>
2016-09-04 03:28:51 +0100

    add, update and delete domain

--- server.js
+++ server.js
@@ -14,23 +14,24 @@ mongoose.connect('mongodb://127.0.0.1:27017/local'); // connect to our database
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-    res.json({ message: 'API endpoint' });   
+    res.json({ message: 'API endpoint' });
 });
+
 
 router.route('/domains')
 
     // create a domain (accessed at POST http://localhost:8080/api/domains)
     .post(function(req, res) {
-        
+
         var domain = new Domain();      // create a new instance of the Domain model
         domain.name = req.body.name;  // set the domains name (comes from the request)
-	domain.expiration = req.body.expiration;
-	domain.creation = req.body.creation;
-	domain.tech = req.body.contactTech;
-	domain.billing = req.body.contactBilling;
-	domain.admin = req.body.contactAdmin;
-	domain.registrant = req.body.registrant;
-	domain.host = req.body.host;
+	      domain.expiration = req.body.expiration;
+	      domain.creation = req.body.creation;
+	      domain.tech = req.body.contactTech;
+	      domain.billing = req.body.contactBilling;
+	      domain.admin = req.body.contactAdmin;
+	      domain.registrant = req.body.registrant;
+	      domain.host = req.body.host;
 
         domain.save(function(err) {
             if (err)
@@ -38,7 +39,7 @@ router.route('/domains')
 
             res.json({ message: 'Domain name created!' });
         });
-        
+
     })
 
     // get all the domains (accessed at GET http://localhost:8080/api/domains)
@@ -51,6 +52,47 @@ router.route('/domains')
         });
     });
 
+
+router.route('/domains/:domain_name')
+// get the domain with that id (accessed at GET http://localhost:8080/api/domains/:domain_name)
+
+    .get(function(req, res) {
+        Domain.findOne(req.params.domain_name, function(err, domain) {
+          if (err)
+            res.send(err);
+
+          res.json(domain);
+        });
+    })
+
+    .put(function(req, res) {
+
+        Domain.findOne(req.params.domain_name, function(err, domain) {
+
+            if (err)
+                res.send(err);
+
+            domain.name = req.body.name;
+
+            domain.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'Domain updated!' });
+            });
+	});
+    })
+
+    .delete(function(req, res) {
+                Domain.remove({
+                    name: req.params.domain_name
+                }, function(err, domain) {
+                    if (err)
+                        res.send(err);
+
+                    res.json({ message: 'Successfully deleted' });
+                });
+    });
 // all of our routes will be prefixed with /api
 app.use('/api', router);
 
@@ -58,4 +100,3 @@ app.use('/api', router);
 // =============================================================================
 app.listen(port);
 console.log('Server is running on port :' + port);
-
